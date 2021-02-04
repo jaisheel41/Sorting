4d4ed4d444297df8b1139edf1354d5ef24f16930
saijaisheelpolimera <saijaisheelpolimera@192.168.1.61>
2021-02-04 11:03:47 +0530

    New Sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,9 +1,30 @@
-
-public class BubbleSort {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+import java.util.Scanner;
+class BubbleSort {
+	public static void main(String []args) {
+		int n, c, d, swap;
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Number of integers to be sorted");
+		
+		n = in.nextInt();
+		int array[] = new int[n];
+		
+		System.out.println("Enter " + n + " integers");
+		
+		for (c = 0; c < n; c++) 
+		      array[c] = in.nextInt();
+		for (c = 0; c < ( n - 1 ); c++) {
+			for (d = 0; d < n - c - 1; d++) {
+				if (array[d] > array[d+1]) 
+				{
+					swap       = array[d];
+					array[d]   = array[d+1];
+					array[d+1] = swap;
+				}
+			}
+		}
+		System.out.println("Sorted list of numbers is");
+		for (c = 0; c < n; c++) 
+		      System.out.println(array[c]);
 	}
-
 }
